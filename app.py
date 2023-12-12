146012df7bacb8c9833202a9e470033f02786aa8
Bartoszbrzoza <148314916+Bartoszbrzoza@users.noreply.github.com>
2023-12-12 09:54:35 +0000

    Pogoda cz1

--- app.py
+++ app.py
@@ -3,9 +3,33 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+import requests
+import json
+import time
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def get_air_quality_data(location):
+    """Pobiera dane o jakości powietrza dla podanej lokalizacji z API OpenAQ."""
+    url = "https://api.openaq.org/v2/measurements?location_id=10566&parameter=o3&parameter=pm25&parameter=bc&parameter=co&parameter=so2&parameter=no2&parameter=pm10&limit=7"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        return data
+    else:
+        return None
+
+def main():
+    """Główna funkcja modułu."""
+    location = "Wroclaw, Lower Silesian Voivodeship, Poland"
+    while True:
+        data = get_air_quality_data(location)
+        if data is not None:
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+            values = []
+            for measurement in data["results"]:
+                values.append({"nazwa-pomiaru": measurement["parameter"], "wartosc [ug/m^3]": measurement["value"]})
+            message = {"location": location, "timestamp": timestamp, "values": values}
+            print(json.dumps(message, indent=4))
+            time.sleep(30)
+
+if __name__ == "__main__":
+    main()
